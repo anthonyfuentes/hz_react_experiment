be19728cf5bc236301c16cf8d41f2e180849de45
Anthony Fuentes <anthony.c.fuentes@gmail.com>
2018-04-04 13:12:43 -0700

    Install React Redux and dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import promise from 'redux-promise'
+import {Provider} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
+import {createStore, applyMiddleware} from 'redux'
+
+import RootReducer from './reducers/RootReducer.js'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App'
 import './style/index.css'
 
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore)
+
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={createStoreWithMiddleware(RootReducer)}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
